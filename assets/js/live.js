8feab9100bfe701cd769e12a9bea8182197b3d64
Street Cat Love <54420067+StreetCatLove@users.noreply.github.com>
2025-05-31 14:11:42 -0400

    Update live.js

--- assets/js/live.js
+++ assets/js/live.js
@@ -1,4 +1,4 @@
-async function checkLivestreamKick() {
+async function checkLivestreamKick(channel) {
             try {
                 // Check cache
                 const cacheKey = `kick_${channel}`;
