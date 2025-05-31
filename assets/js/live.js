6edfdf0f36569419201ed96ba62de557245f51df
Street Cat Love <54420067+StreetCatLove@users.noreply.github.com>
2025-05-31 17:51:22 -0400

    Update live.js

--- assets/js/live.js
+++ assets/js/live.js
@@ -50,44 +50,26 @@ async function checkLivestreamTwitch(channel, container) {
 	                                    return;
                         	}
                         }
-		    	//embed approach
-			const iframe = document.createElement('iframe');
-			iframe.src = `https://player.twitch.tv/?channel=${channel}&parent=${location.hostname}`;
-		    	iframe.channel = `${channel}` ;
-		    	iframe.parent = `${location.hostname}` ;
-			iframe.style.display = 'none';
-			
-			// Event handlers
-			iframe.onload = function() {
-			const player = new Twitch.Player(iframe);
-			
-			player.addEventListener(Twitch.Player.READY, function() {
-			  const isLive = !player.getEnded(); // True if stream is live
-			  console.log(channel, isLive);
-			  // Update cache
-			  localStorage.setItem(cacheKey, JSON.stringify({
-			    isLive,
-			    timestamp: Date.now()
-			  }));
-			
-			  if (isLive) showContainer();
-			  
-			  // Cleanup
-			  setTimeout(function() {
-			    document.body.removeChild(iframe);
-			  }, 1000);
-			});
-			};
-			
-			iframe.onerror = function() {
-				console.log('Twitch embed failed for channel:', channel);
-				document.body.removeChild(iframe);
-			};
-			
-			document.body.appendChild(iframe);
-		
+
+		    	
+		 // API call
+                const response = await fetch(`https://twitch-proxy.freecodecamp.rocks/helix/streams?user_login=${channel}`);
+                if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+                
+                const call = await response.json();
+                console.log( call );
+                const isLive = call.data !== null;
+                
+                // Update cache
+                localStorage.setItem(cacheKey, JSON.stringify({
+                    isLive,
+                    timestamp: now
+                }));
+                
+                if (isLive) showContainer(container);
+                
             } catch (error) {
-                        console.error("Twitch error:", error);
+                console.error("Error checking stream status:", error);
             }
 }
         
